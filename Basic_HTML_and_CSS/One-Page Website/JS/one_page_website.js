59b295143a85b8387393717642773a39630237c5
WatchRinseRepeat <s.a.eyring@gmail.com>
2023-04-12 14:45:52 -0600

    update to one_page_website

---
+++ Basic_HTML_and_CSS/One-Page Website/JS/one_page_website.js
@@ -0,0 +1,41 @@
+//Modal / Light Box
+//Open Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+//Close Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/prev controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thumbnil image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("MySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById('caption');
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
